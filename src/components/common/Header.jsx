c16598e1423b8751719213c2e373057ed8939e5d
Aayshaparveen <aaysha.parveen@ismartapps.co.uk>
2023-06-28 14:32:53 +0530

    Add Some changes

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,73 +1,87 @@
-import React, { useState } from "react"
-import logo from "../assets/images/logo.svg"
-import cartimg from "../assets/images/cart.png"
-import { BiSearch } from "react-icons/bi"
-import { BsBagCheck } from "react-icons/bs"
-import { RiUser3Line } from "react-icons/ri"
-import { AiOutlineHeart, AiOutlineMenu, AiOutlineClose, AiOutlineDelete } from "react-icons/ai"
-import { navlist } from "../assets/data/data"
-import { connect, useDispatch, useSelector } from "react-redux"
-import { Link } from "react-router-dom"
-import { DELETE } from "../../controller/action"
-import { useEffect } from "react"
+/* eslint-disable array-callback-return */
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
+import logo from "../assets/images/logo.svg";
+import cartimg from "../assets/images/cart.png";
+import { BiSearch } from "react-icons/bi";
+import { BsBagCheck } from "react-icons/bs";
+import { RiUser3Line } from "react-icons/ri";
+import {
+  AiOutlineHeart,
+  AiOutlineMenu,
+  AiOutlineClose,
+  AiOutlineDelete,
+} from "react-icons/ai";
+import { navlist } from "../assets/data/data";
+import { connect, useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { DELETE } from "../../controller/action";
+import { useEffect } from "react";
 
 export const Header = () => {
   // navbar
-  const [mobile, setMobile] = useState(false)
+  const [mobile, setMobile] = useState(false);
   // cartopen and close
-  const [cartList, setCartList] = useState(false)
+  const [cartList, setCartList] = useState(false);
   const handleClose = () => {
-    setCartList(null)
-  }
+    setCartList(null);
+  };
   // scroll navbar
   window.addEventListener("scroll", function () {
-    const header = this.document.querySelector(".header")
-    header.classList.toggle("active", this.window.scrollY > 100)
-  })
+    const header = this.document.querySelector(".header");
+    header.classList.toggle("active", this.window.scrollY > 100);
+  });
 
   // cart add in shop
-  const getdata = useSelector((state) => state.cartReducer.carts)
-  console.log(getdata)
+  const getdata = useSelector((state) => state.cartReducer.carts);
+  console.log(getdata);
 
   // delete cart
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const delet = (id) => {
-    dispatch(DELETE(id))
-  }
+    dispatch(DELETE(id));
+  };
 
   // total prcie
-  const [price, setPrice] = useState(0)
-  console.log(price)
+  const [price, setPrice] = useState(0);
+  console.log(price);
 
   const totals = () => {
-    let price = 0
+    let price = 0;
     getdata.map((e, i) => {
-      price = parseFloat(e.price) * e.qty + price
-    })
-    setPrice(price)
-  }
+      price = parseFloat(e.price) * e.qty + price;
+    });
+    setPrice(price);
+  };
 
   useEffect(() => {
-    totals()
-  }, [totals])
+    totals();
+  }, [totals]);
 
   const handleCloses = () => {
-    setCartList(null)
-  }
+    setCartList(null);
+  };
   return (
     <>
-      <header className='header'>
-        <div className='container'>
+      <header className="header">
+        <div className="container">
           <nav>
-            <div className='toggle'>
-              <button onClick={() => setMobile(!mobile)}>{mobile ? <AiOutlineClose className='close heIcon' /> : <AiOutlineMenu className='open heIcon' />}</button>
+            <div className="toggle">
+              <button onClick={() => setMobile(!mobile)}>
+                {mobile ? (
+                  <AiOutlineClose className="close heIcon" />
+                ) : (
+                  <AiOutlineMenu className="open heIcon" />
+                )}
+              </button>
             </div>
-            <div className='left'>
-              <Link to='/'>
-                <img src={logo} alt='logo' />
+            <div className="left">
+              <Link to="/">
+                <img src={logo} alt="logo" />
               </Link>
             </div>
-            <div className='center'>
+            <div className="center">
               <ul className={mobile ? "mobile-nav" : "menu"}>
                 {navlist.map((nav, i) => (
                   <li key={i}>
@@ -77,56 +91,56 @@ export const Header = () => {
               </ul>
             </div>
           </nav>
-          <div className='right'>
-            <div className='right_search'>
-              <input type='text' placeholder='Search Products...' />
-              <BiSearch className='serachIcon heIcon' />
+          <div className="right">
+            <div className="right_search">
+              <input type="text" placeholder="Search Products..." />
+              <BiSearch className="serachIcon heIcon" />
             </div>
-            <div className='right_user'>
-              <RiUser3Line className='userIcon heIcon' />
-              <AiOutlineHeart className='userIcon heIcon' />
+            <div className="right_user">
+              <RiUser3Line className="userIcon heIcon" />
+              <AiOutlineHeart className="userIcon heIcon" />
             </div>
-            <div className='right_card'>
-              <button className='button' onClick={() => setCartList(!cartList)}>
-                <BsBagCheck className='shop heIcon' />
+            <div className="right_card">
+              <button className="button" onClick={() => setCartList(!cartList)}>
+                <BsBagCheck className="shop heIcon" />
                 MY CART<span> ({getdata.length})</span>
               </button>
               <div className={cartList ? "showCart" : "hideCart"}>
                 {getdata.length ? (
-                  <section className='details'>
-                    <div className='details_title'>
+                  <section className="details">
+                    <div className="details_title">
                       <h3>Photo</h3>
                       <p>Product Name</p>
                     </div>
                     {getdata.map((e) => (
-                      <div className='details_content'>
-                        <div className='details_content_img'>
+                      <div className="details_content">
+                        <div className="details_content_img">
                           <Link to={`/cart/${e.id}`} onClick={handleCloses}>
-                            <img src={e.cover} alt='' />
+                            <img src={e.cover} alt="" />
                           </Link>
                         </div>
-                        <div className='details_content_detail'>
-                          <div className='details_content_detail_price'>
+                        <div className="details_content_detail">
+                          <div className="details_content_detail_price">
                             <p>{e.title.slice(0, 20)}...</p>
                             <p>Price : ${e.price}</p>
                             <p>Quantity : {e.qty}</p>
                           </div>
                         </div>
-                        <div className='details_content_detail_icon'>
+                        <div className="details_content_detail_icon">
                           <i onClick={() => delet(e.id)}>
                             <AiOutlineDelete />
                           </i>
                         </div>
                       </div>
                     ))}
-                    <div className='details_total'>
+                    <div className="details_total">
                       <h4>Total : ${price}</h4>
                     </div>
                   </section>
                 ) : (
-                  <div className='empty'>
+                  <div className="empty">
                     <p>Your cart is empty</p>
-                    <img src={cartimg} alt='' />
+                    <img src={cartimg} alt="" />
                   </div>
                 )}
               </div>
@@ -135,11 +149,11 @@ export const Header = () => {
         </div>
       </header>
     </>
-  )
-}
+  );
+};
 const mapStateToProps = (state) => {
   return {
     amount: state.amount,
-  }
-}
-connect(mapStateToProps)(Header)
+  };
+};
+connect(mapStateToProps)(Header);
